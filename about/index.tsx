0d5dc296aa1fdde63295a38552ac9e77e66ef8e2
asuka <yagi.asuka.0x06@gmail.com>
2024-09-06 15:42:14 +0900

    Update about.tsx

--- about/index.tsx
+++ about/index.tsx
@@ -16,8 +16,44 @@ declare global {
 
 export default (_data: Lume.Data, _helpers: Lume.Helpers) => (
   <>
-    <p>準備中．</p>
+    <p>
+      <a href="https://recruit.monicle.co.jp/" target="_blank">株式会社モニクル</a>に所属するソフトウェアエンジニア．
+    </p>
     <h2>著書</h2>
+    <my-book
+      img-alt="「WebAssembly Cookbook vol.1」表紙"
+      img-src="https://techbookfest.org/api/image/w8E01zAy5AwP6GSiUdmwAF.png?size=432"
+    >
+      <h3>WebAssembly Cookbook vol.1</h3>
+      <p>
+        技術書典16にて配布した同人誌．
+        AssemblyScriptとDartを使ったWebAssemblyの使用例などを紹介する本です．
+        <hr />
+        <a
+          href="https://techbookfest.org/product/b0Dp5Remzfe0a6M276Sw0a"
+          target="_blank"
+        >
+          技術書典マーケット
+        </a>
+      </p>
+    </my-book>
+    <my-book
+      img-alt="「ご注文はWASIですか??」表紙"
+      img-src="https://techbookfest.org/api/image/khucwMzFNtMT0ANtsMWdRR.png?size=432"
+    >
+      <h3>ご注文はWASIですか??</h3>
+      <p>
+        技術書典16にて配布した同人誌．
+        「ご注文はWASIですか?」の続編で，WASI 0.2の仕様を紹介する本です．
+        <hr />
+        <a
+          href="https://techbookfest.org/product/8Fshgy7YrSyQGGYkyb4u0a"
+          target="_blank"
+        >
+          技術書典マーケット
+        </a>
+      </p>
+    </my-book>
     <my-book
       img-alt="「ご注文はWASIですか?」表紙"
       img-src="https://techbookfest.org/api/image/6P7WKtLMNJ46NR7P3VHWGD.png?size=432"
@@ -25,12 +61,13 @@ export default (_data: Lume.Data, _helpers: Lume.Helpers) => (
       <h3>ご注文はWASIですか?</h3>
       <p>
         技術書典15にて配布した同人誌．
+        WASI 0.1の仕様を紹介する本です．
         <hr />
         <a
           href="https://techbookfest.org/product/acW1EbS9XxshmBnDUeZtxj"
           target="_blank"
         >
-          購入はこちら
+          技術書典マーケット
         </a>
       </p>
     </my-book>
@@ -44,7 +81,7 @@ export default (_data: Lume.Data, _helpers: Lume.Helpers) => (
         技術書典14にて配布した「WebAssemblyで出来ること」を再編集した内容になります．
         <hr />
         <a href="https://nextpublishing.jp/book/17203.html" target="_blank">
-          購入はこちら
+          購入はこちらから
         </a>
       </p>
     </my-book>
@@ -55,12 +92,13 @@ export default (_data: Lume.Data, _helpers: Lume.Helpers) => (
       <h3>WebAssemblyで出来ること</h3>
       <p>
         技術書典14にて配布した同人誌．
+        WebAssemblyの基本的な仕様を紹介する本です．
         <hr />
         <a
           href="https://techbookfest.org/product/2uwxPNE7mLKksHSFr88X17"
           target="_blank"
         >
-          購入はこちら
+          技術書典マーケット
         </a>
       </p>
     </my-book>
